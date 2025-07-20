d353ff0e97ce28e2e4b622acd9ea9f9c95b1220c
Boardy <charlieboardy@gmail.com>
2025-07-20 15:11:42 +0100

    Fix: Exclude test data from build using next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        // Exclude test data from pdf-parse and similar libraries
+        /^(pdf-parse|mammoth)\/test\/data\/.*
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
