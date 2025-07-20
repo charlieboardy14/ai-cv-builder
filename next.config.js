b94ab667f09eda17ed209902945d9403dc11a8ac
Boardy <charlieboardy@gmail.com>
2025-07-20 15:26:07 +0100

    Fix: Mark pdf-parse and mammoth as external in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.plugins.push(
-        new (require('webpack').IgnorePlugin)({
-          resourceRegExp: /\/(test|tests)\/data\//,
-          contextRegExp: /pdf-parse|mammoth/,
-        })
-      );
+      config.externals.push('pdf-parse', 'mammoth');
     }
     return config;
   },
